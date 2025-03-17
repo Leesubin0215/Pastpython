b7d82a579fe526ca423efeb081294ac483a0a5a6
leesubin <subin8772@naver.com>
2025-03-17 11:54:38 +0900

    2주차 실습 과제

---
+++ 변수.py
@@ -0,0 +1,18 @@
+num1=100
+num2=200
+resSum= num1 + num2
+print(num1,"+",num2,"=",resSum)
+
+빼기=num1- num2
+print(num1,"-", num2, "=", 빼기)
+
+e= num1 * num2
+print(num1,"*", num2, "=", e)
+
+f= num1/ num2
+print(num1,"/", num2, "=", f)
+
+str1= "hello"
+str2= "world"
+print(str1+" "+str2)
+print(str1*3)
